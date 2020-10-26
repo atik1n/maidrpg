aab0a001f28b608e0f7bd9564e516bc525c8f7cd
Christopher Lytle <lytle.chris88@gmail.com>
2020-10-26 18:04:48 -0500

    Update #2 Master Character sheet

--- module/sheets/MaidRPGCharacterSheet.js
+++ module/sheets/MaidRPGCharacterSheet.js
@@ -23,6 +23,12 @@ export default class MaidRPGCharacterSheet extends ActorSheet{
     if (this.actor.data.type == 'maid') {
       this._prepareCharacterItems(data);
     }
+    if (this.actor.data.type == 'butler') {
+      this._prepareCharacterItems(data);
+    }
+    if (this.actor.data.type == 'master') {
+      this._prepareCharacterItems(data);
+    }
 
     return data;
   }
